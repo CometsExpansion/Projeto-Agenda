81ac6345815a21ea7201cf33aeda983e5e5e7799
CometsExpansion <alosantos.1035@gmail.com>
2024-08-17 13:05:03 -0300

    17 de agosto

--- contact/admin.py
+++ contact/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
-from contact.models import Contact
+from contact.models import Contact, Categoria
 
+# Recebe como argumento uma classe que criei
+# Decora uma classe que personaliza como serão mostrados os contatos na /admin
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('id', 'first_name', 'last_name', 'phone', )
+    ordering = ('id',)
+    search_fields = ('first_name', 'id')
+    list_per_page = 10 
+    list_max_show_all = 20
+    list_editable = ('first_name', 'last_name', 'phone',)
+    list_display_links = ('id', )
+    
 
-# admin.site.register(Contact)
-# Register your models here.
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+# # Outra forma de fazer, só que sem a personalização
+# # admin.site.register(Contact)
+# # Outra forma de fazer, com a personalização
+# # admin.site.register(Contact, ContactAdmin)
--- contact/models.py
+++ contact/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 
 # id (primary key)
 # first_name (string), last_name (string),  phone(string)
@@ -10,6 +10,17 @@ from django.utils import timezone
 # category (foreign key), show (boolean), owner (foreign key)
 # picture (imagem)
 
+class Categoria(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f'{self.name}' 
+    
+
+    
 class Contact (models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -17,6 +28,14 @@ class Contact (models.Model):
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(Categoria, on_delete=models.SET_NULL,
+                                  blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True,
+                              null=True)
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}' 
+    
+
